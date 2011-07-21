db059108c5cbf31456411ad4083ae08ff5b5a868
xiaoxin:wq <593748946@qq.com>
2011-07-21 11:07:54 +0800

    加上定义

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main(int argc, const char *argv[])
 {
-   printf("hello everyone\n");
-   return 0;
+ int i;
+ for (i = 0; i < 10000; i++) {
+  	
+	 printf("hello everyone\n");
+  }
+  	 return 0;
 }
