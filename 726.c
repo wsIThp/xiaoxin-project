6abd10dd7dd6abdaa06a1d113d979b1ab898e2fe
xiaoxin:wq <593748946@qq.com>
2011-07-27 16:53:57 +0800

    数值转化

---
+++ 726.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(int argc, const char *argv[])
+{
+
+    unsigned int a=0x556677;
+    unsigned int mask = (1<<31);    
+    int  i = 0 ;
+
+
+    for (i = 0; i < 32; i++) {
+        printf("%c",((a & mask)>0)?'1':'0');
+        mask >> 1;
+    }
+
+    
+    printf("\n");
+    return 0;
+}
---
+++ test1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(int argc, const char *argv[])
+{
+  char c=100;
+  b=
+  
+  printf("b=%d\n",b);
+
+   
+    return 0;
+}
