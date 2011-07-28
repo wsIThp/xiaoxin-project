fd3b51cb102851cc3f9d46ce67c6f6d742afdba8
xiaoxin:wq <593748946@qq.com>
2011-07-28 16:51:43 +0800

    OK

---
+++ shuzu_test072803.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define N 15
+int main(int argc, const char *argv[])
+{
+    int a[N] = {0};
+    int i=0;
+    /*for (i = 0; i < 50; i++) {
+        a[i]==0;
+        printf("%d\n",a[i]);
+    }*/
+    if(i==0)
+    {
+        for (i = 0; i <50 ; i++) 
+        {
+         i+=3;
+         a[N]==1;
+         printf("%d\n",i);
+         }
+    }
+    else
+    {
+        return 0;
+    }
+
+    /*printf("%d\n",i);*/
+    return 0;
+}
