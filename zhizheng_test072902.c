6eaa7f866dabf87a3716c5d64a656c54d2f70f2b
xiaoxin:wq <593748946@qq.com>
2011-07-29 09:16:59 +0800

    指针联系的简化版

---
+++ zhizheng_test072902.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define N  20
+int main(int argc, const char *argv[])
+{
+    char str[N]="abcdefghijklmn";
+    char *ptr;
+  
+    ptr=str;
+
+    /*for (i = 0; i < N; i++,ptr++)
+    {
+        *ptr='a'+i;
+    }
+    *ptr='\0';//
+    printf("str=%s\n",str);//
+
+    for (i = 0; i < N; i++)
+    {
+        printf("%c",str[i]);
+    }
+    printf("\n");*/
+    while(*ptr!='\0')
+    {
+        printf("%c",*ptr);
+        ptr++;
+    }
+    printf("\n");
+    return 0;
+}
