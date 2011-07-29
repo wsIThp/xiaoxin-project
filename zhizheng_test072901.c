e75ec4a5f9e2a8bffd27822dc71c99970b033089
xiaoxin:wq <593748946@qq.com>
2011-07-29 09:06:36 +0800

    字符窜输出1

---
+++ zhizheng_test072901.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define N  20
+int main(int argc, const char *argv[])
+{
+    char str[N];
+    char *ptr;
+    int i=0;
+    ptr=str;
+    for (i = 0; i < N; i++,ptr++)
+    {
+        *ptr='a'+i;
+    }
+    *ptr='\0';//
+    printf("str=%s\n",str);//
+
+    /*for (i = 0; i < N; i++)
+    {
+        printf("%c",str[i]);
+    }
+    printf("\n");*/
+    return 0;
+}
