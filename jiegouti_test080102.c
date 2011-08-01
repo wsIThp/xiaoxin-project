1adceec989e6ec55d27b1ee8006016d1a8ff7433
xiaoxin:wq <593748946@qq.com>
2011-08-01 11:32:22 +0800

    结构体

---
+++ jiegouti_test080102.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+struct date
+{
+    int a;
+    char b;
+    char c;
+    int d;
+};
+
+int main(int argc, const char *argv[])
+{
+    struct date m ={11,'b','v',25};
+    struct date *p=&m;
+    printf("%d\n",sizeof (struct date));
+    printf("%d\n",m.a); 
+    printf("%d\n",(*p).a);
+    printf("%d\n",p->a);
+    printf("%d\n",m.b);
+    printf("%d\n",m.c);
+    printf("%d\n",m.d);
+    return 0;
+}
+
+/*#include <stdio.h>
+#include <string.h>
+
+struct student
+{
+    int number;
+    char name[20];
+    struct student *p;  ///////
+};
+
+
+typedef struct student STU;
+int main (void)
+{
+    STU m;
+    STU n;          //////
+    STU *ptr=NULL;
+
+    ptr=&m;
+    ptr->number=20;
+    strcpy(ptr->name,"zhang" );
+
+    printf("");
+    printf("%-5d  %s\n",ptr->number,ptr->name);
+    return 0;
+}*/
+
+
