5e07e4283bddca9bfc79a2b10d09af79414438c4
xiaoxin:wq <593748946@qq.com>
2011-08-20 18:12:08 +0800

    case实例

---
+++ case_shili.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+	int count	;
+	int *to;
+	int *from;
+	register n = (count + 7) / 8;  
+	    switch (count % 8)
+		    {
+			    case 0:    do { *to = *from++;
+							    	case 7:     *to = *from++;
+			 					    case 6:     *to = *from++;
+							    	case 5:     *to = *from++;
+							        case 4:     *to = *from++;
+			       				    case 3:     *to = *from++;
+			  	     			    case 2:     *to = *from++;
+							        case 1:     *to = *from++;
+	                           } while (--n > 0);
+			 }
+	return 0;
+}
